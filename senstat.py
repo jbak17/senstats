b2fcffa3a18c2cb7711e82da12dae9bddd2fb94e
jbak17 <jbak17@student.monash.edu>
2016-07-07 16:26:00 +1000

    - Docx functions working - Now PDF doesn't work. - seems to be returning all the text of a pdf file somewhere.

--- senstat.py
+++ senstat.py
@@ -14,7 +14,7 @@ import PyPDF2
 from pdf_to_txt import convert_pdf_to_txt
 import outstrings
 import zipfile
-import lxml.etree
+from bs4 import BeautifulSoup
 
 #iterates over files and calls helper functions.
 def hearings(path):
@@ -29,79 +29,55 @@ def hearings(path):
 
     printer = outstrings.Outstrings() #create Outstrings ojbect with the functionality of different print outputs
 
-    #convert all files to txt
     #gather relevant files from directory
     files = list ( set ( glob.glob (path+'/*.pdf') + glob.glob (path+'/*.docx') ) )
+
     for item in files:
-        print 'Converting file {} of {}'.format((files.index(item) + 1), len(files))
-        conv_to_txt(item)
+        if item[-3:] == 'pdf':
+            stats = pdf_reader(item)
+        elif item[-3:] == 'ocx':
+            stats = docx_reader(item) #returns tuple (ctteeType, duration, pages, location, witnesses)
+        else:
+            print 'Unable to identify file type of', item
 
-    #get files for processing
-    files_txt = glob.glob (path+'/*.txt')
-    #print 'The following text files were found: \n {}'.format(files)
-
-    #calculate durations, hearing number and witnesses:
-    for path in files_txt:
-        #type of committee
-        #print 'establish cttee type'
-        ctteeType = cttee_type(path)
-        #print 'calculate duration'
-        time = hearing_duration(path)
-        #print 'find location'
-        loc = hearing_location(path)    #find location of hearing
-        if ctteeType == 'References':
-            ref_cttee['hearings'] += 1      #increment number of references hearings
-            print 'count witnesses'
-            ref_cttee['witnesses'] += witness_count(path)       #add witnesses
-            #get page count
-            try:
-                pdf_path = path[:-3] + 'pdf'
-                doc_path = path[:-3] + 'docx'
-                if pdf_path in files:
-                    ref_cttee['hansard'] += hansard_page_count(pdf_path)
-                elif doc_path in files:
-                    print 'doc found'
-                    ref_cttee['hansard'] += pages_from_docx(doc_path)
-                else:
-                    print 'Unable to calculate pages for {}'.format(path)
-            except Exception as e:
-                print 'Unable to calculate pages for {}'.format(path)
-            #update time
-            if type(time) == float:
-                ref_cttee['duration'] += time
-            else:
-                print time      #this is an error message saying that the file couldn't be calculated
-            if len(loc) < 5:
-                ref_cttee['locations'][loc] += 1
-            else:
-                print loc
-
-        elif ctteeType == 'Legislation':
+        if stats[0] == 'Legislation':
             leg_cttee['hearings'] += 1
-            leg_cttee['witnesses'] += witness_count(path)
-            #get page count
-            try:
-                pdf_path = path[:-3] + 'pdf'
-                leg_cttee['hansard'] += hansard_page_count(pdf_path)
-            except Exception as e:
-                print '{} does not exist'.format(path)
-            try:
-                doc_path = path[:-3] + 'docx'
-                leg_cttee['hansard'] += pages_from_docx(doc_path)
-            except Exception as e:
-                print 'Unable to calculate pages for {}'.format(doc_path)
-            if type(time) == float:
-                leg_cttee['duration'] += hearing_duration(path)
-            else:
-                print time
-            if len(loc) < 5:
-                leg_cttee['locations'][loc] += 1
-            else:
-                print loc
+            leg_cttee['duration'] += stats[1]
+            leg_cttee['hansard'] += stats[2]
+            leg_cttee['locations'][stats[3]] += 1
+            leg_cttee['witnesses'] += stats[4]
+        else:
+            ref_cttee['hearings'] += 1
+            ref_cttee['duration'] += stats[1]
+            ref_cttee['hansard'] += stats[2]
+            ref_cttee['locations'][stats[3]] += 1
+            ref_cttee['witnesses'] += stats[4]
+        print stats
 
-        #print
+    #print results
     printer.publicHearingOutString(leg_cttee)
     printer.publicHearingOutString(ref_cttee)
+
+def pdf_reader(PDF_file):
+    pages = hansard_page_count(PDF_file)
+
+    nf = convert_pdf_to_txt(PDF_file)
+    ctteeType = cttee_type(nf)
+    duration = hearing_duration(nf)
+    location = hearing_location(nf)
+    witnesses = witness_count(nf)
+    return (ctteeType, duration, pages, location, witnesses)
+
+def docx_reader(docx_file):
+    ctteeType, pages, witnesses = getPagesAndCtteeType(docx_file)
+    duration = 0
+    location = None
+
+    nf = word_to_txt(docx_file, 'txt')
+    duration = hearing_duration(nf)
+    location = hearing_location(nf)
+
+    return (ctteeType, duration, pages, location, witnesses)
 
 def conv_to_txt(path):
     '''
@@ -121,25 +97,16 @@ def conv_to_txt(path):
             print 'Unable to convert %s to pdf'.format(path)
     elif path[-3:] == 'doc' or 'ocx':
         try:
-            pdfPath = word_to_txt(path, 'pdf')
-            txtPath = pdfPath.split('.')
-            txtPath = txtPath[0] + '.txt'
+            txtPath = path[:-4] + '.txt'
             fo = open(txtPath, 'w');
             fo.write(convert_pdf_to_txt(pdfPath));
             fo.close();
         except Exception as e:
             print 'Unable to convert %s to pdf'.format(path)
-
-def pages_from_docx(docx_file):
-    #try:
-    cmd = 'unzip -p {} docProps/app.xml | grep -oP "(?<=\<Pages\>).*(?=\</Pages\>)"'.format(docx_file)
-    print cmd
-    pages = subprocess.check_output(cmd)
-    print type(pages)
-    pages -= 4 #adjusting page count due to Hansard formatting
-    return pages
-    #except Exception as e:
-    print 'Unable to extract page numbers from {}'.format(docx_file)
+    else:
+        print 'Unknown file type'
+        print 'conv_to_txt function problem'
+    return fo
 
 def cttee_type(file):
     '''
@@ -164,7 +131,7 @@ def cttee_type(file):
                     continue
     return cttee
 
-def hearing_location(path):
+def hearing_location(txt_file):
     '''
     Determines which state or territory the hearing was held in.
     Takes a path to a txt file.
@@ -176,7 +143,7 @@ def hearing_location(path):
     location = None;
     start = False;
     retString = None;
-    with open(path) as f:
+    with open(txt_file) as f:
         for line in f:
             line = line.rstrip();
             line = line.lstrip();
@@ -205,7 +172,6 @@ def witness_count(path):
     '''
     witnesses = 0
     witness_list = []
-    #print 'witness list path: ' + path
     with open(path) as f:
         for line in f:
             line = line.rstrip();
@@ -278,12 +244,12 @@ def word_to_txt(path, export_format):
     print '{} converted to {}.'.format(path, newpath)
     return newpath
 
-def hansard_page_count(file):
+def hansard_page_count(PDF_file):
     '''
     takes a pdf file and returns an integer sum of the number of pages.
     '''
     pages = 0
-    inputFile = PyPDF2.PdfFileReader(file)
+    inputFile = PyPDF2.PdfFileReader(PDF_file)
     pages += inputFile.getNumPages()
     pages -= 4 #taking into account the leading pages in Hansard pdfs.
     return pages
@@ -334,38 +300,41 @@ def get_files(dir, type):
 def getPagesAndCtteeType(path):
     # open zipfile
     zf = zipfile.ZipFile(path)
-
-    #create trees from two xml files
-    app = lxml.etree.fromstring(zf.read('docProps/app.xml'))
-    custom = lxml.etree.fromstring(zf.read('docProps/custom.xml'))
+    data = {'cttee_type' : None, 'pages' : 0, 'witnesses': 0}
 
     #get page numbers
-    for element in app.iter():
-        #print("%s - %s" % (element.tag, element.text))
-        #print type(element.text)
-        string = element.tag
-        string = string.replace('{http://schemas.openxmlformats.org/officeDocument/2006/extended-properties}',"")
-        if string == 'Pages':
-            print element.text + '\n'
+    app = zf.read('docProps/app.xml')
+    pages = re.findall('<Pages>[0-9]+<', app)
+    try:
+        pagestr = pages[0]
+    except Exception as e:
+        'Unable to establish number of pages for {}'.format(path)
+    trans = string.maketrans("","")
+    nodigs = trans.translate(trans, string.digits)
+    outString = pagestr.translate(trans, nodigs)
+    data['pages'] = int(outString) - 4 #subtract 4 to account for Hansard styling.
 
-    cttee = None
-    for element in custom.iter():
-       # print("%s - %s" % (element.tag, element.text))
-        #get references/leg cttee
-        keys = element.keys()
-        try:
-            if keys[1] == 'pid':
-                PID = element.get(keys[1])
-            if PID == '17':
-                #print element
-                child = element.getchildren()
-                cttee = child[0].text
-        except:
-            continue
-    cttee = cttee.split()
-    cttee = int(cttee[1]) - 4 #subtract four due to hansard formatting.
-    print cttee
+    #get cttee type
+    custom = zf.read('docProps/custom.xml')
+    app = zf.read('docProps/custom.xml')
+    cttee = re.findall('(Legislation|References)', app)
+    try:
+        data['cttee_type'] = cttee[0]
+    except:
+        'Unable to establish committee type for {}'.format(path)
+
+    #get number of witnesses. item4 contains data on witnesses.
+    try:
+        witnesses = zf.read('customXml/item4.xml')
+        soup = BeautifulSoup(witnesses, 'lxml')
+        witList = len(soup.find_all('t:name'))
+        data['witnesses'] = witList
+    except Exception as e:
+        'Unable to establish number of witnesses for {}'.format(path)
+
+    return (data['cttee_type'], data['pages'], data['witnesses'],)
 
 if __name__ == '__main__':
     #print count_subs(get_files())
-    hearings(sys.argv[1])
+    # hearings(sys.argv[1])
+    hearings('/home/jarrod/workspace/senstats/test_docs/activeTest')
